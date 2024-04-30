0bf734ed79c0a70190341fda66963eb5ee84d2ca
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-04-30 13:23:18 -0300

    chore: add husky and lint staged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-md">Home page</h1>
+    <main className="bg-zinc flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-[50px]">Home page</h1>
     </main>
   );
 }
