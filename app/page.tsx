a8c80fcf47b31f312db787616af9eb4ce60417a7
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-05-02 22:10:09 -0300

    feat: add haeder component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./_components/header";
+
 export default function Home() {
   return (
-    <main className="bg-zinc flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-[50px]">Home page</h1>
+    <main>
+      <Header></Header>
     </main>
   );
 }
