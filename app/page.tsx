a8ffcdd8ccac3a4f49ba15eaa03cb09d715722a0
Werbeth Araujo <werbeth1991pbs@gmail.com>
2024-05-05 11:21:29 -0300

    feat: add restaurand list and restaurant item components

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import ProductList from "./_components/product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import Banner from "./_components/banner";
+import RestaurantList from "./_components/restaurant-list";
 
 export default async function Home() {
   const products = await db.product.findMany({
@@ -48,6 +49,20 @@ export default async function Home() {
         <ProductList products={products} />
       </div>
       <Banner src="/promo-banner-02.png" alt="Lanches a partir de 17,90" />
+
+      <div className="space-y-4 py-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes Recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
+            Ver mais
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <RestaurantList />
+      </div>
     </main>
   );
 }
